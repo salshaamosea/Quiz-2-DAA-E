ec57ac7cb1ad7cf0da3d24f68910fdcf904fb499
salshaamosea <salshaamosea@gmail.com>
2019-03-28 21:50:00 +0700

    Update 1

--- Quiz 2/AppDelegate.swift
+++ Quiz 2/AppDelegate.swift
@@ -8,19 +8,52 @@
 
 import Cocoa
 
+enum Cell {
+    case empty
+    case blocked
+    case start
+    case goal
+    case path
+    func color() -> CGColor {
+        switch (self) {
+        case .empty: return NSColor.white.cgColor
+        case .blocked: return NSColor.black.cgColor
+        case .start: return NSColor.green.cgColor
+        case .goal: return NSColor.red.cgColor
+        case .path: return NSColor.yellow.cgColor
+        }
+    }
+}
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
+
+func == (lhs: Point, rhs: Point) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
+
+//class MazeView: NSView {
+//    
+//}
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
+
+

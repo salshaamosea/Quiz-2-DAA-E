9b82f7ff493f1e0cc46cd755decaaf1a65aa0b7e
salshaamosea <salshaamosea@gmail.com>
2019-03-28 14:22:00 +0700

    First commit

---
+++ Quiz 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quiz 2
+//
+//  Created by Salsha Armenia Amosea on 30/03/19.
+//  Copyright © 2019 Salsha Armenia Amosea. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
